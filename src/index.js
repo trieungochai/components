4fc8f9067dc8b4bb77ba6fd62dc97545a423b8b6
hai.trieu <trieungochai.dev@gmail.com>
2021-05-11 17:57:17 +0700

    Showing custom children

--- src/index.js
+++ src/index.js
@@ -2,23 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 import faker from "faker";
 
+import ApprovalCard from "./ApprovalCard";
 import CommentDetail from "./CommentDetail";
+
+// Live Reloading
+if (module.hot) {
+  module.hot.accept();
+}
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail
-        author="Rei"
-        timeAgo="Today at 4:45PM"
-        content="Nice blog post"
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <CommentDetail
+          author="Rei"
+          timeAgo="Today at 4:45PM"
+          content="Nice blog post"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+
       <CommentDetail
         author="Kai"
         timeAgo="Today at 2:20AM"
         content="I like the subject"
         avatar={faker.image.avatar()}
       />
+
       <CommentDetail
         author="Futoi"
         timeAgo="Today at 5:00PM"
